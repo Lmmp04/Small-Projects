08e00b9f62462f0c5704f45ae6019f155fdf9469
Leo Platti <78324368+Lmmp04@users.noreply.github.com>
2023-12-01 01:59:06 -0500

    password_generator.py
    Very simple 22 character password generator. Operated in a terminal via the "python password_generator.py" command

---
+++ password_generator.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+# This script was made by Lmmp04 and uploaded to github.
+
+import random
+
+characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '*', '&']
+
+print ("Here is your newly generated password:")
+print ('\n')
+print ("".join(random.sample(characters,22)))
